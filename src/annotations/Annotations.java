80820ca373fffd9356abc8cdac3520d475099b90
Michael Baker <michael.a.baker34@gmail.com>
2021-03-18 14:25:36 -0500

    add suppresswarnings example

--- src/annotations/Annotations.java
+++ src/annotations/Annotations.java
@@ -1,5 +1,8 @@
 package annotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Annotations {
 
     public static void main(String[] args) {
@@ -8,5 +11,15 @@ public class Annotations {
         p.newMethod();
         Child c = new Child();
         c.theMethodToSayWhatItIs();
+
+
     }
+
+    @SuppressWarnings({"unchecked", "deprecation"})
+    public static void doRiskyThings() {
+        Child c = new Child();
+        c.oldMethod(); // deprecated static access
+        List<Integer> l = new ArrayList();
+    }
+
 }
