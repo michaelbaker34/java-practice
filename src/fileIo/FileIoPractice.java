076327d66aa453e80a49d97d24e6f9b0f46c7dcb
Michael Baker <michael.a.baker34@gmail.com>
2021-03-27 17:59:51 -0500

    add filepractice

--- src/fileIo/FileIoPractice.java
+++ src/fileIo/FileIoPractice.java
@@ -1,22 +1,11 @@
+package fileIo;
+
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.nio.file.*;
 import java.util.*;
 
 public class FileIoPractice {
-
-/*  - write code in the main method that prints the following messages:
-    “Justin works at Codeup.”
-    “Casey works at Codeup.”
-    “Ry works at Codeup.”
-    “David works at Codeup.”
-    - create an array list of new names for the people in your breakout room
-    - add code to your main method that replaces the contents of `people.txt`
-    with the people in your breakout room
-    - set the contents of the text file back to Codeup staff
-    - create a static method, `deleteEntry`, that takes in the name of someone (a string)
-    and when invoked, rewrites a new list to the `people.txt` file with all entries
-    except the passed in name (bonus: make the name to delete case insensitive) */
 
     private static final String directory = "fileIo";
     private static final String filename = "people.txt";
@@ -40,7 +29,7 @@ public class FileIoPractice {
 
     public static void main(String[] args) {
 
-        deleteEntry("Justin");
+//        deleteEntry("Justin");
 
 //        String directory = "fileIo";
 //        String filename = "people.txt";
@@ -59,7 +48,7 @@ public class FileIoPractice {
 //        }
 
 // create new lines of strings
-//        List<String> newLines = Arrays.asList("Sabrina", "Emily", "Yasin", "Ann");
+        List<String> newLines = Arrays.asList("Sabrina", "Emily", "Yasin", "Ann");
 // try to write new lines to file at path p
 //        try {
 //            Files.write(p, newLines);
